dce06b2cad656b70eedc03d6a4ecedcf04ff2cf6
Leonel Ortega <leonel.ortega@tucambista.com>
2023-10-25 02:22:46 -0500

    homeScreen version 1

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hola esclavo'),
+
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      drawer: SideMenu( scaffoldKey: scaffoldKey ),
+      appBar: AppBar(
+        title: const Text('Products'),
+        actions: [
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon( Icons.search_rounded)
+          )
+        ],
+      ),
+      body: const _ProductsView(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Nuevo producto'),
+        icon: const Icon( Icons.add ),
+        onPressed: () {},
+      ),
     );
+  }
+}
+
+
+class _ProductsView extends StatelessWidget {
+  const _ProductsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('no quiero vivir'));
   }
 }
