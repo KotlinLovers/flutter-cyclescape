4512cec37e1520073b8472c6492ca4c1aef3296a
Stevens <u20201e735@upc.edu.pe>
2023-11-16 14:21:43 -0500

    feat: changing shopping cart distribution

--- lib/presentation/screens/shopping_cart_screen.dart
+++ lib/presentation/screens/shopping_cart_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cyclescape/shared/util/shared_entities/bicycle_shopping_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
@@ -37,44 +37,26 @@ class _ShoppingCartScreenState extends State<ShoppingCartScreen> {
           ? const Center(
               child: Text("Carrito vacío...", style: TextStyle(fontSize: 20)),
             )
-          : CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  expandedHeight: 50.0,
-                  floating: false,
-                  pinned: true,
-                  title: Row(
-                    children: [
-                      Text("Total: $totalPrice"),
-                      Spacer(),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Row(
-                          children: [
-                            Text("Continuar compra"),
-                            Icon(Icons.arrow_forward_ios)
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      final bicycle = bicycles![index];
-                      return GestureDetector(
-                        onTap: () {
-                          context.push('/bicycle/${bicycle.bicycleId}');
-                        },
-                        child: Card(
-                          elevation: 1,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            children: [
-                              ListTile(
+          : Consumer(
+              builder: (context, value, child) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: bicycles!.length,
+                        itemBuilder: (context, index) {
+                          final bicycle = bicycles![index];
+                          return Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(12.0, 5, 12.0, 5),
+                            child: Container(
+                              decoration:
+                                  BoxDecoration(color: Colors.grey.shade100),
+                              child: ListTile(
+                                onTap: () {
+                                  context.push('/bicycle/${bicycle.bicycleId}');
+                                },
                                 leading: Image.network(
                                   bicycle.imageData,
                                   fit: BoxFit.contain,
@@ -101,15 +83,50 @@ class _ShoppingCartScreenState extends State<ShoppingCartScreen> {
                                       color: Colors.red),
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(8)),
+                        padding: EdgeInsets.fromLTRB(14, 5, 14, 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text("Total:"),
+                                Spacer(),
+                                Text("$totalPrice")
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  minimumSize: MaterialStateProperty.all<Size>(
+                                      Size(double.infinity, 40.0)),
+                                  padding: MaterialStateProperty.all<
+                                          EdgeInsetsGeometry>(
+                                      EdgeInsets.symmetric(horizontal: 4)),
+                                ),
+                                onPressed: () {},
+                                child: Text("Continuar compra"),
+                              ),
+                            ),
+                          ],
                         ),
-                      );
-                    },
-                    childCount: bicycles!.length,
-                  ),
-                ),
-              ],
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
     );
   }
